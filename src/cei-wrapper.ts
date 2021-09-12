cd19e592edaf15174991cc7498170d36fcd70247
Michael Santillan <me@michaelsantillan.com>
2021-09-12 19:57:21 -0300

    Refactor CeiWrapper to use authentication service

--- src/cei-wrapper.ts
+++ src/cei-wrapper.ts
@@ -1,20 +1,29 @@
-import { AuthenticationContext } from "./application/contracts/authentication-context/authentication-context";
+import "reflect-metadata";
 import { Result } from "./application/contracts/result/result";
 import { createContainer } from "./infrastructure/configurations/container";
 import { ISettings } from "./infrastructure/configurations/settings";
+import { AuthenticationService } from "./services/authentication/authentication-service";
 import { CeiService } from "./services/cei/cei-service";
 import { ConsolidatedValues } from "./services/cei/entities/consolidated-value";
 
 export class CeiWrapper {
+  private readonly authenticationService: AuthenticationService;
   private readonly ceiService: CeiService;
 
   constructor(settings: ISettings) {
-    this.ceiService = createContainer(settings).get(CeiService);
+    const container = createContainer(settings);
+
+    this.authenticationService = container.get(AuthenticationService);
+    this.ceiService = container.get(CeiService);
   }
 
   public async getConsolidatedValues(): Promise<Result<ConsolidatedValues>> {
-    const authenticationContext = new AuthenticationContext("", "");
+    const authenticationResult = await this.authenticationService.login();
 
-    return await this.ceiService.getConsolidatedValues(authenticationContext);
+    if (authenticationResult.isError) {
+      return authenticationResult;
+    }
+
+    return await this.ceiService.getConsolidatedValues(authenticationResult.data);
   }
 }
--- tests/unit/cei-wrapper.spec.ts
+++ tests/unit/cei-wrapper.spec.ts
@@ -1,15 +1,18 @@
 import { plainToClass } from "class-transformer";
 import { ResultSuccess } from "../../src/application/contracts/result/result-success";
 import { CeiWrapper } from "../../src/cei-wrapper";
+import { AuthenticationContext } from "../../src/services/authentication/entities/authentication-context";
 import { ConsolidatedValues } from "../../src/services/cei/entities/consolidated-value";
 
 let getConsolidatedValuesMock: jest.Mock;
+let loginMock: jest.Mock;
 
 jest.mock("../../src/infrastructure/configurations/container", () => ({
   createContainer: () => ({
     get: function () {
       return {
         getConsolidatedValues: getConsolidatedValuesMock,
+        login: loginMock,
       };
     },
   }),
@@ -21,6 +24,7 @@ describe("CeiWrapper", () => {
       const resultMock = plainToClass(ConsolidatedValues, {});
 
       getConsolidatedValuesMock = jest.fn().mockResolvedValue(new ResultSuccess(resultMock));
+      loginMock = jest.fn().mockResolvedValue(new ResultSuccess(new AuthenticationContext("", "")));
 
       const ceiWrapper = new CeiWrapper({ username: "", password: "" });
 
