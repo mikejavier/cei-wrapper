43314f4fe3d3695edc0b5ab17e13dd6eb765688b
Michael Santillan <me@michaelsantillan.com>
2021-09-14 21:33:35 -0300

    Update demo script

--- scripts/demo.ts
+++ scripts/demo.ts
@@ -1,13 +1,57 @@
 import { CeiWrapper } from "../src/cei-wrapper";
 
 (async () => {
-  const ceiWrapper = new CeiWrapper({ username: "", password: "" });
+  const authenticateContext = await CeiWrapper.authenticateUser({
+    anticaptchaServiceKey: "KEY",
+    username: "USER",
+    password: "PASS",
+  });
+
+  if (authenticateContext.isError) {
+    console.log("fail to authenticate user", authenticateContext);
+    // {
+    //   errorMessage: "Error Message";
+    //   isError: true;
+    //   status: 0;
+    // }
+
+    return;
+  }
+
+  console.log(authenticateContext.data);
+  // const authenticateContext = {
+  //   cacheId: "f8f7cac9-5b99-4160-94ae-eddd5dd2218",
+  //   token: "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiZmFrZSIsIklzc3VlciI6Iklzc3VlciIsIlVzZXJuYW1lIjoiZmFrZSIsImV4cCI6MTYzMTY2NTM2MSwiaWF0IjoxNjMxNjY1MzYxfQ.WtIZtZkcnG-vtd72eAP0S9nYpOAprcg3jdoQc8xv1es",
+  // };
+
+  const ceiWrapper = new CeiWrapper(authenticateContext.data);
 
   const result = await ceiWrapper.getConsolidatedValues();
 
   if (result.isError) {
     console.log("We have some problem!", result);
+    // {
+    //   errorMessage: "Error Message";
+    //   isError: true;
+    //   status: 0;
+    // }
+    return;
   }
 
-  console.log(result);
+  console.log(result.data);
+  // {
+  //   "total": "1000.50",
+  //   "values": [
+  //     {
+  //       "product": "Renda Variável",
+  //       "totalAmount": "600.54",
+  //       "percentage": "0.607"
+  //     },
+  //     {
+  //       "product": "Tesouro Direto",
+  //       "totalAmount": "300.92",
+  //       "percentage": "0.393"
+  //     }
+  //   ]
+  // }
 })();
